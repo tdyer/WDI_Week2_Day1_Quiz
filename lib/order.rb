ba67063c587a84e0307fdebe4f1ff0ab6f0f50de
Rahul Horé <hore.rahul@gmail.com>
2013-09-30 11:39:06 -0400

    Initializes Order class

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,14 @@
-require_relative '<fill_me_in>'
-require_relative '<fill_me_in>'
+require_relative 'product_type'
+require_relative 'order_item'
 
 class Order
+  attr_accessor :id, :items
+
+  def initialize(id, items)
+    @id    = id
+    @items = items
+  end
+
+  def complete_xaction
+  end
 end
