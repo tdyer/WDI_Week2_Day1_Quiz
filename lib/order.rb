ef4a6ae7b3654153cf32ca631d5969115679fc81
Abby Howell <abby.g.howell@gmail.com>
2013-09-30 11:27:30 -0400

    updated order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,14 @@
-require_relative '<fill_me_in>'
-require_relative '<fill_me_in>'
+require_relative 'order_item'
+require_relative 'product_type'
 
 class Order
+
+  def initialize(order_id, *order_items)
+  	@order_id = order_id
+  	@order_items = []
+  end
+
+  def complete_xaction
+  end
+
 end
