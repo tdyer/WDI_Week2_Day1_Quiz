0e4f38a8f10c29906f63b647faf94bd3a1e6ae65
Abby Howell <abby.g.howell@gmail.com>
2013-09-30 11:42:33 -0400

    updated order.rb adding log function

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,36 @@
 require_relative 'order_item'
 require_relative 'product_type'
+require_relative 'log'
 
 class Order
+	include Log
 
-  def initialize(order_id, *order_items)
-  	@order_id = order_id
+	@@sum_of_all_orders = 0
+	@@order_count = 0
+
+	attr_accessor :order_id, :order_items
+
+  def initialize(*order_items)
+  	order_id_generator
   	@order_items = []
+  	@@sum_of_all_orders += @price
   end
 
-  def complete_xaction
+  def order_id_generator
+  	@order_id = @@order_count + 1
   end
+
+  def to_s
+  	@string = "#{@order_id}: #{@order_items}"
+  end
+
+  def complete_xaction()
+  	Log::entry(@string)
+  end
+
+  def price
+  	return @price
+  end
+
 
 end
