c0bdf030fc5ef3526877ac4dc154a501ee1d3b81
Blake Ruddock <ruddock.blake@gmail.com>
2013-09-30 12:43:29 -0400

    Added ability to calculate cost of one order

--- bin/make_orders.rb
+++ bin/make_orders.rb
@@ -11,19 +11,22 @@ products << peas << carrots << milk << pudding
 
 # Create an order
 order_number = 30
-order = Order.new(order_number, "Jack Flash")
-order.order_items << OrderItem.new(2, pudding, 0.65)
-order.order_items << OrderItem.new(10, peas, peas.price - 0.1)
-order.complete_xaction
+order1 = Order.new(order_number, "Jack Flash")
+order1.order_items << OrderItem.new(2, pudding, 0.65)
+order1.order_items << OrderItem.new(10, peas, peas.price - 0.1)
+order1.complete_xaction
 order_number += 1
 
 # Create another order
-order = Order.new(order_number, "Tom Menino")
-order.order_items << OrderItem.new(6, milk, milk.price)
-order.order_items << OrderItem.new(300, peas, peas.price - 0.1)
-order.order_items << OrderItem.new(7, carrots, carrots.price)
-order.complete_xaction
+order2 = Order.new(order_number, "Tom Menino")
+order2.order_items << OrderItem.new(6, milk, milk.price)
+order2.order_items << OrderItem.new(300, peas, peas.price - 0.1)
+order2.order_items << OrderItem.new(7, carrots, carrots.price)
+order2.complete_xaction
 order_number += 1
 
 
 # puts "Total sales = #{Order.total_sales}"
+
+
+order1.order_total
