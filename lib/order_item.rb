285c7252d5d436689d0f4724f9dbdb728cd6b9a9
Ben Winter <ben.g.winter@gmail.com>
2013-09-30 11:30:01 -0400

    created order_item and product_type class

--- lib/order_item.rb
+++ lib/order_item.rb
@@ -1,6 +1,13 @@
 require_relative '<fill_me_in>'
 
 class OrderItem
+  attr_accessor :product, :quantity, :unit_price
+
+  def initialize
+    @product = product
+    @quantity = quantity
+    @unit_price = unit_price  
+  end
 
   def to_s
     "product type: #{@product}, quantity: #{@quantity}, unit_price: #{@unit_price}"
