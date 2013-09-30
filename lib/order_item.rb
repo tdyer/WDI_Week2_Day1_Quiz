6d35a4edafe582da46a8fbb7390c76d4c4ed7bf1
Abby Howell <abby.g.howell@gmail.com>
2013-09-30 11:25:42 -0400

    updated order_item.rb

--- lib/order_item.rb
+++ lib/order_item.rb
@@ -1,6 +1,14 @@
-require_relative '<fill_me_in>'
+require_relative 'product_type'
 
 class OrderItem
+	attr_accessor :product_type, :quantity, :unit_price
+
+	def initialize(product_type, quantity, unit_price)
+		@product_type = product_type
+		@quantity = quantity
+		@unit_price = unit_price
+	end
+
 
   def to_s
     "product type: #{@product}, quantity: #{@quantity}, unit_price: #{@unit_price}"
