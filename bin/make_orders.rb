a1c60e243050c7f5f3fa267faa99006d18edcd67
Blake Ruddock <ruddock.blake@gmail.com>
2013-09-30 11:53:05 -0400

    Finished phase 1

--- bin/make_orders.rb
+++ bin/make_orders.rb
@@ -5,7 +5,7 @@ products = [ ]
 peas = ProductType.new(120, "Peas", 2.25, "Small bag of Peas")
 carrots = ProductType.new(121, "Carrots", 3.60, "Large bag of Carrots")
 milk = ProductType.new(122, "Milk", 4.25, "1 gallon")
-pudding = ProductType.new(123, "Pudding", 0.75)
+pudding = ProductType.new(123, "Pudding", 0.75, "Case of pudding")
 products << peas << carrots << milk << pudding
 
 
@@ -22,7 +22,6 @@ order = Order.new(order_number, "Tom Menino")
 order.order_items << OrderItem.new(6, milk, milk.price)
 order.order_items << OrderItem.new(300, peas, peas.price - 0.1)
 order.order_items << OrderItem.new(7, carrots, carrots.price)
-
 order.complete_xaction
 order_number += 1
 
--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,24 @@
-require_relative '<fill_me_in>'
-require_relative '<fill_me_in>'
+require_relative 'order_item'
+require_relative 'product_type'
 
 class Order
+
+	include Log
+	
+	attr_accessor :order_id, :customer_name, :order_items
+
+	def initialize(order_id, customer_name)
+		@order_id = order_id
+		@customer_name = customer_name
+		@order_items = []
+	end
+
 end
+
+
+
+# - Order
+#   - And order should have an order id
+#   - And order should have one or more order items.
+#   - And order should have a complete_xaction method that will log (see
+#     below)..
