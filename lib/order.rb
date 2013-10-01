05df736092ed6a3483141379691b3dc85403f99a
Rahul Horé <hore.rahul@gmail.com>
2013-09-30 20:39:08 -0400

    Implements cumulative sales

--- lib/order.rb
+++ lib/order.rb
@@ -1,9 +1,16 @@
 require_relative 'log'
 require_relative 'order_item'
+require 'pry'
 
 class Order
   include Log
   attr_accessor :order_id, :customer, :order_items
+
+  @@cumulative_sales = 0.0
+
+  def self.cumulative_sales
+    @@cumulative_sales
+  end
 
   def initialize(order_id, customer)
     @order_id    = order_id
@@ -17,13 +24,15 @@ class Order
 
   def order_price
     @order_items.inject(0.0) do |total, item|
-      total + item.quantity * item.product.price
+      total += (item.quantity * item.product.price)
+      binding.pry
     end
   end
 
   def to_s
     "Order ID: #{@order_id}
     Customer: #{@customer}
-    Item(s): #{@order_items}"
+    Item(s): #{@order_items}
+    Total Price: $#{order_price}"
   end
 end
