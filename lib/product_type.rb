17d45ebc118ed2b7afd15fb824736a3b69bf8983
Rahul Horé <hore.rahul@gmail.com>
2013-09-30 21:07:30 -0400

    Includes Log module in ProductType

--- lib/product_type.rb
+++ lib/product_type.rb
@@ -1,16 +1,19 @@
-
+require_relative 'log'
 # This is a description of a *type* of product
 class ProductType
+  include Log
+  
   attr_accessor :id, :name, :price, :description
 
-  def initialize(id, name, price, description = 'item')
+  def initialize(id, name, price, description = 'N/A')
     @id          = id
     @name        = name
     @price       = price
     @description = description
+    log "Creating product type: #{self}"
   end
 
   def to_s
-    "Product #{@id}: #{name}: #{description}: $#{price}"
+    "Product #{@id}: #{@name}: #{@description}: $#{@price}"
   end
 end
